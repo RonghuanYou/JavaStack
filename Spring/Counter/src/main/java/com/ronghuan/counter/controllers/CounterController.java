a9a329b558400bb003b86c42932d25692dd85310
RonghuanYou <ronghuanyou@gmail.com>
2021-08-08 00:13:04 -0700

    init commit

---
+++ Spring/Counter/src/main/java/com/ronghuan/counter/controllers/CounterController.java
@@ -0,0 +1,37 @@
+package com.ronghuan.counter.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CounterController {
+	
+	@RequestMapping("/")
+	public String index(HttpSession session, Model model) {
+		// SET SESSION(IF COUNT IS NOT ALREADY IN SESSION)
+		if (session.getAttribute("count").equals(null)) {
+			// INITIALIZE COUNT IN SESSION
+			session.setAttribute("count", 0);
+		} else {
+			// INCREASE THE COUNT BY 1 USING GET/SET
+			Integer currentCount = (Integer) session.getAttribute("count");
+			currentCount += 1;
+			// SET NEW COUNT INTO SESSION 
+			session.setAttribute("count", currentCount);
+			// PASS PARAMETER
+			model.addAttribute("currentCount", currentCount);
+		}
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/counter")
+	public String counter(HttpSession session, Model model) {
+		Integer currentCount = (Integer) session.getAttribute("count");
+		model.addAttribute("currentCount", currentCount);
+		return "counter.jsp";
+	}
+	
+}
---
+++ Spring/Counter/src/main/webapp/WEB-INF/counter.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+	<meta charset="UTF-8">
+	<title>Insert title here</title>
+</head>
+<body>
+	<div class="container">
+		<p>You have visited <a href="/">http://localhost:8080/</a> <c:out value ="${currentCount}"/> times</p>
+		<p><a href="/">Test another visit?</a></p>		
+	</div>
+</body>
+</html>
